47f2cee9d7c614b3c4d267d9daffe9848c3dd3c9
Dmitriy <Malnev.Dmitriy@wildberries.ru>
2019-04-30 13:35:15 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# RecipeBox
+Проект RecipeBox более подробную ифномарцию и ТЗ можно найти по ссылке [learn.freecodecamp.org](https://learn.freecodecamp.org/coding-interview-prep/take-home-projects/build-a-recipe-box) 
+
+## Angular
+
+Решение данной задачи решил сделать на фреймворке Angular v7.0
+
+## Для заупска проекта
+
+> `npm i` <br/>
+> `ng serve -o` - автоматически откроется в вашем браузере.
+
---
+++ src/app/components/recipe/recipe.component.ts
@@ -0,0 +1,81 @@
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from "../../models/recipe";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+
+@Component({
+  selector: 'app-recipe',
+  templateUrl: './recipe.component.html',
+  styleUrls: ['./recipe.component.less']
+})
+export class RecipeComponent implements OnInit {
+
+  isOpenDialog = false;
+
+  recipes: Array<Recipe> = [
+    {
+      id: 1,
+      name: 'Борщ',
+      ingredients: 'Мясо, картофель, луковица, морковь, свекла, капуста, чеснок, томатная паста, уксус, соль, перец'
+    },
+    {
+      id: 2,
+      name: 'Солянка',
+      ingredients: 'Колбаса копченая, сосиски, говядина вареная, бульонные кубики, вода, лук репчатый, масло растительное, томат пюре, маслины, сметана, огурцы маринованые'
+    }
+  ];
+
+  data:FormGroup = null;
+
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.data = new FormGroup(
+      {
+        name: new FormControl(
+          '', [Validators.required]
+        ),
+        ingredients: new FormControl(
+          '', [Validators.required]
+        )
+      }
+    );
+  }
+
+  openDialog() {
+    this.isOpenDialog = true;
+  }
+
+  closeDialog() {
+    this.isOpenDialog = false;
+  }
+
+  deleteRecipe(itemId: any) {
+    this.recipes = this.recipes.filter(x => x.id !== itemId);
+  }
+
+  updateRecept(itemUpdate: any) {
+    this.recipes.forEach((item) => {
+      if (item.id === itemUpdate.id) {
+        let item = JSON.parse(JSON.stringify(itemUpdate));
+      }
+    });
+  }
+
+  saveNewReceipt() {
+    if (this.data.valid) {
+      const recipeNew: Recipe = {
+        id: this.recipes.length + 1,
+        name: this.data.get('name').value,
+        ingredients: this.data.get('ingredients').value
+      };
+      this.recipes.push(recipeNew);
+      localStorage.setItem('recipe', JSON.stringify(this.recipes));
+      this.closeDialog();
+      this.data.reset();
+    }
+  }
+
+}
